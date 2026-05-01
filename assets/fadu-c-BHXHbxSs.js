40a2f4f26998ffe10fdbfcae03d43c174c07f171
akane <107654710+genericness@users.noreply.github.com>
2026-05-01 04:08:00 +0000

    deploy 2026-05-01T04:07:59Z

---
+++ assets/fadu-c-BHXHbxSs.js
@@ -0,0 +1,6 @@
+import{c as n}from"./fadu-c-B9atkPoM.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"a53afbc30353198aaeb614d8c1a64f753d981ed5"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},f=new e.Error().stack;f&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[f]="b2f4f663-fa93-4333-9fb0-cfa98780eb4e",e._sentryDebugIdIdentifier="sentry-dbid-b2f4f663-fa93-4333-9fb0-cfa98780eb4e")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=n("arrow-left",d);export{t as A};
