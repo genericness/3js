69f131fad91311780a1c56e01425518f7f061a70
akane <107654710+genericness@users.noreply.github.com>
2026-05-01 02:44:51 +0000

    deploy 2026-05-01T02:44:50Z

---
+++ assets/vhgr-c-UpnMVIGr.js
@@ -0,0 +1,6 @@
+import{c as n}from"./vhgr-c-DYTE8k8w.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"2a36911c0170db1f0b23f2e3f1999c2bca68528b"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="dbbb30e4-9e58-467d-849c-569802af6526",e._sentryDebugIdIdentifier="sentry-dbid-dbbb30e4-9e58-467d-849c-569802af6526")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=n("arrow-left",o);export{t as A};
