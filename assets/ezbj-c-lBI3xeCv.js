127de2972ce3e9485a229e52e40606689fb143e0
akane <107654710+genericness@users.noreply.github.com>
2026-05-07 06:34:53 +0000

    deploy 2026-05-07T06:34:52Z

---
+++ assets/ezbj-c-lBI3xeCv.js
@@ -0,0 +1,6 @@
+import{c as n}from"./ezbj-c-BTG7l8wY.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"7c4a7ae592cb5083e420cdb6788b1c1b5d8c5532"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},a=new e.Error().stack;a&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[a]="abec3cc3-6f89-45c5-bc33-6a62ad9a74bc",e._sentryDebugIdIdentifier="sentry-dbid-abec3cc3-6f89-45c5-bc33-6a62ad9a74bc")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z",key:"r04s7s"}]],o=n("star",d);export{o as S};
