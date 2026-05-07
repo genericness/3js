4fff1648996bb98c55302e5b2fd252bf494a2a42
akane <107654710+genericness@users.noreply.github.com>
2026-05-07 07:12:10 +0000

    deploy 2026-05-07T07:12:09Z

---
+++ assets/oqwg-c-rKMB-RG4.js
@@ -0,0 +1,6 @@
+import{c as n}from"./oqwg-c-CFsHSr4B.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"f1dc77858c63f9503ed973c2eeb123259a99a87a"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="30a92f8d-0b17-4f41-a6d7-aeac65e93abf",e._sentryDebugIdIdentifier="sentry-dbid-30a92f8d-0b17-4f41-a6d7-aeac65e93abf")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],a=n("arrow-left",f);export{a as A};
