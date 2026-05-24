a83105e8187ede1f61ed2df5bb092faa041f8eb1
akane <107654710+genericness@users.noreply.github.com>
2026-05-24 17:21:52 +0000

    deploy 2026-05-24T17:21:51Z

---
+++ assets/qlsb-c-BLqJQuSK.js
@@ -0,0 +1,6 @@
+import{c as d}from"./qlsb-c-CONkqTCP.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},f=new e.Error().stack;f&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[f]="477fb41c-3258-4bdf-8dbf-c5f053afcf66",e._sentryDebugIdIdentifier="sentry-dbid-477fb41c-3258-4bdf-8dbf-c5f053afcf66")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=d("arrow-left",n);export{t as A};
