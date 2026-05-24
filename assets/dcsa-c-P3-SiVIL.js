706920b0f06ca0378668924704fa43a72209ae5a
akane <107654710+genericness@users.noreply.github.com>
2026-05-24 17:24:29 +0000

    deploy 2026-05-24T17:24:28Z

---
+++ assets/dcsa-c-P3-SiVIL.js
@@ -0,0 +1,6 @@
+import{c as f}from"./dcsa-c-Bsa9dZF6.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="a80f984a-44fb-4b45-9e87-66797aa04fe0",e._sentryDebugIdIdentifier="sentry-dbid-a80f984a-44fb-4b45-9e87-66797aa04fe0")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],a=f("arrow-left",d);export{a as A};
