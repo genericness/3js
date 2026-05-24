d6cca831fbe44eaa07148dc10b911a41b5c72c00
akane <107654710+genericness@users.noreply.github.com>
2026-05-24 17:24:24 +0000

    deploy 2026-05-24T17:24:23Z

---
+++ assets/yeau-c-Dt6-N9m2.js
@@ -0,0 +1,6 @@
+import{c as d}from"./yeau-c-Bw63HCH4.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="59b52f8b-cb9e-465e-8c11-d1b3624b7a9f",e._sentryDebugIdIdentifier="sentry-dbid-59b52f8b-cb9e-465e-8c11-d1b3624b7a9f")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=d("arrow-left",o);export{t as A};
