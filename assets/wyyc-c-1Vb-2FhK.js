51566f40c4cd31e1b29e642f58bca4ea2cf25797
akane <107654710+genericness@users.noreply.github.com>
2026-05-24 17:25:15 +0000

    deploy 2026-05-24T17:25:14Z

---
+++ assets/wyyc-c-1Vb-2FhK.js
@@ -0,0 +1,6 @@
+import{c as n}from"./wyyc-c-D7Dxvzu3.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="16f67e44-5216-46b7-9a5a-2ddcf26fa277",e._sentryDebugIdIdentifier="sentry-dbid-16f67e44-5216-46b7-9a5a-2ddcf26fa277")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=n("arrow-left",f);export{t as A};
