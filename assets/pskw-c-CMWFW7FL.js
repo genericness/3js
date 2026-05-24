b4aaab7a6dd68f20bd43ee386adeef64dfec6c97
akane <107654710+genericness@users.noreply.github.com>
2026-05-24 17:24:40 +0000

    deploy 2026-05-24T17:24:39Z

---
+++ assets/pskw-c-CMWFW7FL.js
@@ -0,0 +1,6 @@
+import{c as n}from"./pskw-c-jlS-tmMa.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="9bad1ad6-425c-4aaa-adc0-7818567ed89f",e._sentryDebugIdIdentifier="sentry-dbid-9bad1ad6-425c-4aaa-adc0-7818567ed89f")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],f=n("arrow-left",a);export{f as A};
