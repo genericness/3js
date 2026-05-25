bfcd813baadb6031e0adb6f15f9034782df22a04
akane <107654710+genericness@users.noreply.github.com>
2026-05-25 03:43:20 +0000

    deploy 2026-05-25T03:43:19Z

---
+++ assets/kkhu-c-DTSyGPfs.js
@@ -0,0 +1,6 @@
+import{c as n}from"./kkhu-c-CIrCkjk4.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="a17996c1-49e0-41f3-8b5c-6da792da723c",e._sentryDebugIdIdentifier="sentry-dbid-a17996c1-49e0-41f3-8b5c-6da792da723c")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=n("arrow-left",o);export{t as A};
