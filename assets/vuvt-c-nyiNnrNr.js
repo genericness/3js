b378857bc1cdc0c7f3538037687aee9b7ecb777c
akane <107654710+genericness@users.noreply.github.com>
2026-05-25 03:43:36 +0000

    deploy 2026-05-25T03:43:35Z

---
+++ assets/vuvt-c-nyiNnrNr.js
@@ -0,0 +1,6 @@
+import{c as n}from"./vuvt-c-9z9f1yJR.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8573a2898b27ef3599d125a41f6b5ca31756576c"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="47333833-a125-4eef-91e9-5043811d189d",e._sentryDebugIdIdentifier="sentry-dbid-47333833-a125-4eef-91e9-5043811d189d")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],t=n("arrow-left",o);export{t as A};
