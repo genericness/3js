99d88c10121962678c5e7c55bc89f705b3e026dc
akane <107654710+genericness@users.noreply.github.com>
2026-05-30 03:40:34 +0000

    deploy 2026-05-30T03:40:33Z

---
+++ assets/pzfa-c-DuvMVqt7.js
@@ -0,0 +1,6 @@
+import{c as n}from"./pzfa-c-CE4p_meh.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"2dbade56efe6df94c2ca80a7f8a1d839697626a4"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="59d16892-7b04-40d1-92ca-aa31eab1f36a",e._sentryDebugIdIdentifier="sentry-dbid-59d16892-7b04-40d1-92ca-aa31eab1f36a")})()}catch{}/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],f=n("arrow-left",a);export{f as A};
