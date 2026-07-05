06276164a363c076ffd90d9642528c295b551cea
akane <107654710+genericness@users.noreply.github.com>
2026-07-05 07:17:19 +0000

    deploy 2026-07-05T07:17:18Z

---
+++ assets/dmkp-c-DMYSNPYq.js
@@ -0,0 +1,1 @@
+import{k as i}from"./dmkp-c-BCpIZwgP.js";import"./dmkp-c-D2zwYr1l.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
