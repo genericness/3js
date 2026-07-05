64f7b5775d0734723640b93d572bdbd50512d392
akane <107654710+genericness@users.noreply.github.com>
2026-07-05 08:43:31 +0000

    deploy 2026-07-05T08:43:30Z

---
+++ assets/xbnc-c-Cx-_TAwR.js
@@ -0,0 +1,1 @@
+import{k as i}from"./xbnc-c-BXKe9H7K.js";import"./xbnc-c-BkGcGlRt.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
