29e5d26013297e831669ebf6d8d42761654f80f4
akane <107654710+genericness@users.noreply.github.com>
2026-07-05 08:41:37 +0000

    deploy 2026-07-05T08:41:36Z

---
+++ assets/kvwt-c-DWANKJ1R.js
@@ -0,0 +1,1 @@
+import{k as i}from"./kvwt-c-8n38LK75.js";import"./kvwt-c-Ebbg5Jzm.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
