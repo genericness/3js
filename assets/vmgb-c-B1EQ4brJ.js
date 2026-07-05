003a46fb8b9b2dfd292e16f75d0b1b58c1f1a883
akane <107654710+genericness@users.noreply.github.com>
2026-07-05 08:41:09 +0000

    deploy 2026-07-05T08:40:48Z

---
+++ assets/vmgb-c-B1EQ4brJ.js
@@ -0,0 +1,1 @@
+import{k as i}from"./vmgb-c-D4F6d4xJ.js";import"./vmgb-c-BdtE8R9S.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
