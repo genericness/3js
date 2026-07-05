feed806bca299e5c2788e54f505c00182a2201d2
akane <107654710+genericness@users.noreply.github.com>
2026-07-05 06:55:46 +0000

    deploy 2026-07-05T06:55:45Z

---
+++ assets/prsg-c-BwMiBjE3.js
@@ -0,0 +1,1 @@
+import{k as i}from"./prsg-c-D97dTppe.js";import"./prsg-c-hwgXkXK8.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
