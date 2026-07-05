d2867d6102dd4ca6e022648a1c4bbb874480cd9e
akane <107654710+genericness@users.noreply.github.com>
2026-07-05 07:17:31 +0000

    deploy 2026-07-05T07:17:30Z

---
+++ assets/kalt-c-BWXTyPOt.js
@@ -0,0 +1,1 @@
+import{k as i}from"./kalt-c-W8r4P4yP.js";import"./kalt-c-CxMj6r1N.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
