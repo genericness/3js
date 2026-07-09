2a954a3f05b369224dc69c7d52e8356a6a98d48f
akane <107654710+genericness@users.noreply.github.com>
2026-07-09 08:32:45 +0000

    deploy 2026-07-09T08:32:43Z

---
+++ assets/qizy-c-mRA_bNIU.js
@@ -0,0 +1,1 @@
+import{k as i}from"./qizy-c-CU2MRlkf.js";import"./qizy-c-CVGDqCne.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
