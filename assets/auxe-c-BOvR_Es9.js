4a99315ec2b296e1c62e4bb5eec1c213392a98f1
akane <107654710+genericness@users.noreply.github.com>
2026-07-09 08:34:12 +0000

    deploy 2026-07-09T08:34:10Z

---
+++ assets/auxe-c-BOvR_Es9.js
@@ -0,0 +1,1 @@
+import{k as i}from"./auxe-c-C4N3CdpK.js";import"./auxe-c-BIx4N-hF.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
