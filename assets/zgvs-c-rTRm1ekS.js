2aa795001a5424c06b632c65711eba532dddd421
akane <107654710+genericness@users.noreply.github.com>
2026-07-22 21:52:16 +0000

    deploy 2026-07-22T21:52:14Z

---
+++ assets/zgvs-c-rTRm1ekS.js
@@ -0,0 +1,1 @@
+import{k as i}from"./zgvs-c-Du2KuvM5.js";import"./zgvs-c-B4kLw2BL.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
