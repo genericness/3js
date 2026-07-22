6dfe2c381a089ba4e07586a017a1d45be7a73ab2
akane <107654710+genericness@users.noreply.github.com>
2026-07-22 22:22:23 +0000

    deploy 2026-07-22T22:22:22Z

---
+++ assets/yqfk-c-Dn84Gz0D.js
@@ -0,0 +1,1 @@
+import{k as i}from"./yqfk-c-6Ha9nPIo.js";import"./yqfk-c-D4sYFEe1.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
