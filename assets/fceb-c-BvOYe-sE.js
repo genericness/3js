91c71c7db46f18d86c921a0be21eb7890d383fef
akane <107654710+genericness@users.noreply.github.com>
2026-07-22 03:58:20 +0000

    deploy 2026-07-22T03:58:20Z

---
+++ assets/fceb-c-BvOYe-sE.js
@@ -0,0 +1,1 @@
+import{k as i}from"./fceb-c-8bhnJIl2.js";import"./fceb-c-DT9WXO1L.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
