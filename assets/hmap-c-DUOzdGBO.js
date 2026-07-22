0d3399d315b5d8ab856b341aed55925b9d12c406
akane <107654710+genericness@users.noreply.github.com>
2026-07-22 22:10:08 +0000

    deploy 2026-07-22T22:10:07Z

---
+++ assets/hmap-c-DUOzdGBO.js
@@ -0,0 +1,1 @@
+import{k as i}from"./hmap-c-GhY2HBhc.js";import"./hmap-c-YF1cux_X.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
