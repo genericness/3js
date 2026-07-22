d96ec5971bf3deba9a6522784cadfd14c58f6c97
akane <107654710+genericness@users.noreply.github.com>
2026-07-22 21:52:46 +0000

    deploy 2026-07-22T21:52:45Z

---
+++ assets/ahme-c-BvypAhh5.js
@@ -0,0 +1,1 @@
+import{k as i}from"./ahme-c-nXA14bbX.js";import"./ahme-c-DklPFIp4.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
