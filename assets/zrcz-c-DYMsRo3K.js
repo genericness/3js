2391c9bdff8c7aed0a9b4076928648ffe9578c21
akane <107654710+genericness@users.noreply.github.com>
2026-08-02 06:03:47 +0000

    deploy 2026-08-02T06:03:45Z

---
+++ assets/zrcz-c-DYMsRo3K.js
@@ -0,0 +1,1 @@
+import{k as i}from"./zrcz-c-DKyBnl0g.js";import"./zrcz-c-O5JmMDLO.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
