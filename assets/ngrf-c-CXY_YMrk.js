d2f459ee9ed44d294ceeb363eb017d17eb26e14d
genericness <107654710+genericness@users.noreply.github.com>
2026-08-09 14:34:58 +0000

    deploy 2026-08-09T14:34:58Z

---
+++ assets/ngrf-c-CXY_YMrk.js
@@ -0,0 +1,1 @@
+import{k as i}from"./ngrf-c-D9Vm6ddV.js";import"./ngrf-c-C_jVCc1o.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
