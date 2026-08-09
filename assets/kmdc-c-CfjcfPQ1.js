4e6d69238d71c96648090dc686a00838735a3bc0
genericness <107654710+genericness@users.noreply.github.com>
2026-08-09 15:21:33 +0000

    deploy 2026-08-09T15:21:33Z

---
+++ assets/kmdc-c-CfjcfPQ1.js
@@ -0,0 +1,1 @@
+import{k as i}from"./kmdc-c-B4XoPxX4.js";import"./kmdc-c-DHHB_o3r.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
