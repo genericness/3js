ff4a3ff7b91dd6d33746a5aefbdde611fb909066
genericness <107654710+genericness@users.noreply.github.com>
2026-08-19 04:49:07 +0000

    deploy 2026-08-19T04:49:07Z

---
+++ assets/wbcu-c-C-wVrtgp.js
@@ -0,0 +1,1 @@
+import{k as i}from"./wbcu-c-D6R_nkrf.js";import"./wbcu-c-DFq31oxj.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
