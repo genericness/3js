a350a34d87307837bec3e6a79ff7a0be6987f6fa
genericness <107654710+genericness@users.noreply.github.com>
2026-08-19 04:47:29 +0000

    deploy 2026-08-19T04:47:29Z

---
+++ assets/ybyo-c-BPduBFVg.js
@@ -0,0 +1,1 @@
+import{k as i}from"./ybyo-c-83T2_Glg.js";import"./ybyo-c-Byoo8GRm.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
