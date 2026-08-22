3460e4b194e88061724091ee08e148b069b227a4
genericness <107654710+genericness@users.noreply.github.com>
2026-08-22 08:23:38 +0000

    deploy 2026-08-22T08:23:38Z

---
+++ assets/lkwq-c-BZoDBfQZ.js
@@ -0,0 +1,1 @@
+import{k as i}from"./lkwq-c-B5CGdqc4.js";import"./lkwq-c-C_zFuE-D.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
