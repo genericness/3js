2c6c5589f0dfb60a3d11ca248c75d4de48f89f3e
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 06:26:01 +0000

    deploy 2026-08-24T06:26:01Z

---
+++ assets/ocnc-c-tE7_Kl-Y.js
@@ -0,0 +1,1 @@
+import{k as i}from"./ocnc-c-Cqop6tow.js";import"./ocnc-c-DKmMF92u.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
