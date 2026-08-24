bcf11bcf979b250e4ac996ed8572cd5f66be03e9
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 05:52:09 +0000

    deploy 2026-08-24T05:52:09Z

---
+++ assets/oxxl-c-CvlBeXYQ.js
@@ -0,0 +1,1 @@
+import{k as i}from"./oxxl-c-Bm9H6Gaz.js";import"./oxxl-c-oR3n9uC1.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
