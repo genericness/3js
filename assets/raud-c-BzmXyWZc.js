99b89ca9b1c4beca0236f7f384409957fb033a9a
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 06:51:27 +0000

    deploy 2026-08-24T06:51:27Z

---
+++ assets/raud-c-BzmXyWZc.js
@@ -0,0 +1,1 @@
+import{k as i}from"./raud-c-By2N3NID.js";import"./raud-c-g_CQVkOb.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
