d2e49e71b233396066a0681312b801b4670a8ad2
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 05:49:41 +0000

    deploy 2026-08-24T05:49:41Z

---
+++ assets/zode-c-B-on7n4d.js
@@ -0,0 +1,1 @@
+import{k as i}from"./zode-c-B5pIy83J.js";import"./zode-c-u0kUf6En.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
