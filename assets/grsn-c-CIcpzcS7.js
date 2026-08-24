9dcedabb23508a9108e2fc159c07d8faaa010632
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 06:40:13 +0000

    deploy 2026-08-24T06:40:13Z

---
+++ assets/grsn-c-CIcpzcS7.js
@@ -0,0 +1,1 @@
+import{k as i}from"./grsn-c-Ts6vfoV9.js";import"./grsn-c-D8vkS91r.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
