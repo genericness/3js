e563f0b2153dfe5042bf681ce7a0c0ab3df95686
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 06:49:52 +0000

    deploy 2026-08-24T06:49:52Z

---
+++ assets/xeum-c-DdP9cUXV.js
@@ -0,0 +1,1 @@
+import{k as i}from"./xeum-c-CMp0xdMU.js";import"./xeum-c-BbTUmJZi.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
