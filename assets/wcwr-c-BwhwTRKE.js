02902ac4f596c9fe0d94f9510a471f2948d5f45d
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 06:22:57 +0000

    deploy 2026-08-24T06:22:57Z

---
+++ assets/wcwr-c-BwhwTRKE.js
@@ -0,0 +1,1 @@
+import{k as i}from"./wcwr-c-BmGrKZyF.js";import"./wcwr-c-DPK1YYx1.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
