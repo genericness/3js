7f620e8c291bfb6bd78a07fff96d85ff4aff8808
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 06:25:30 +0000

    deploy 2026-08-24T06:25:30Z

---
+++ assets/emdy-c-BSUgVJ2g.js
@@ -0,0 +1,1 @@
+import{k as i}from"./emdy-c-DQX7A2_S.js";import"./emdy-c-LKjrzM7E.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
