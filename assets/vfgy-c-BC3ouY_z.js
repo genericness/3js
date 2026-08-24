50945e1e4b6c620278f817a883833fa4f6420910
genericness <107654710+genericness@users.noreply.github.com>
2026-08-24 05:26:43 +0000

    deploy 2026-08-24T05:26:43Z

---
+++ assets/vfgy-c-BC3ouY_z.js
@@ -0,0 +1,1 @@
+import{k as i}from"./vfgy-c-CIaT11V_.js";import"./vfgy-c-Du5AfW1G.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
