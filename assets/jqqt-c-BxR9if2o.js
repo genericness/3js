5b42261f28e943b14e5863ccad9467f7a402e098
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 05:21:47 +0000

    deploy 2026-09-06T05:21:47Z

---
+++ assets/jqqt-c-BxR9if2o.js
@@ -0,0 +1,1 @@
+import{k as i}from"./jqqt-c-C2lYOrXZ.js";import"./jqqt-c-Dkc6pEjx.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
