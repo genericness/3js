7f95f4bb8104230ca258807d317197ea9ae7d005
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 02:38:54 +0000

    deploy 2026-09-06T02:38:54Z

---
+++ assets/qyag-c-BnDcQqG-.js
@@ -0,0 +1,1 @@
+import{k as i}from"./qyag-c-BpI54MtZ.js";import"./qyag-c-Y_jxdKij.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
