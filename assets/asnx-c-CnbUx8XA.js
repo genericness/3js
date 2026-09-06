b16e62bd45749a0bb1e2c8a04c9cda6fcd553958
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 02:41:59 +0000

    deploy 2026-09-06T02:41:59Z

---
+++ assets/asnx-c-CnbUx8XA.js
@@ -0,0 +1,1 @@
+import{k as i}from"./asnx-c-D8v8lY2G.js";import"./asnx-c-nb8lLt_q.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
