b8d4706293262879cfd82bbcf1e6834419796074
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 02:37:08 +0000

    deploy 2026-09-06T02:37:08Z

---
+++ assets/soui-c-BWF6ufoC.js
@@ -0,0 +1,1 @@
+import{k as i}from"./soui-c-C_vG-Ve3.js";import"./soui-c-HeAYi96R.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
