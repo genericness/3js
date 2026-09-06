d03b9e05aa4224319628feff369d870c6e703f67
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 02:35:22 +0000

    deploy 2026-09-06T02:35:22Z

---
+++ assets/hywo-c-DFEjyNBD.js
@@ -0,0 +1,1 @@
+import{k as i}from"./hywo-c-BB32Onf6.js";import"./hywo-c-DdyehVqu.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
