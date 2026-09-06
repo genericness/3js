051b7dc036bd28069796c69fdd8dcece499f77c4
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 05:26:01 +0000

    deploy 2026-09-06T05:26:01Z

---
+++ assets/ueej-c-p0dmG8og.js
@@ -0,0 +1,1 @@
+import{k as i}from"./ueej-c-yB2S6j0t.js";import"./ueej-c-CZWFel8M.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
