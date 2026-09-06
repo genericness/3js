b05ab7bc75c5465b678b3ca01141d6c53e92136c
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 02:42:53 +0000

    deploy 2026-09-06T02:42:53Z

---
+++ assets/rngi-c-Bo3qaBbu.js
@@ -0,0 +1,1 @@
+import{k as i}from"./rngi-c-DIXGcbwY.js";import"./rngi-c-BKQwj-cp.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
