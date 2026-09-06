0f004299433bdf662fc2793da243af89e41ed818
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 05:26:29 +0000

    deploy 2026-09-06T05:26:29Z

---
+++ assets/mmcu-c-DEcEzxAT.js
@@ -0,0 +1,1 @@
+import{k as i}from"./mmcu-c-DH-RWQ3Y.js";import"./mmcu-c-DHEt3xSX.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
