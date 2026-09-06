82caaefe4bcbc15ddca5b9dc62ae660b1876f497
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 00:28:07 +0000

    deploy 2026-09-06T00:28:07Z

---
+++ assets/tlpl-c-0xzij7N9.js
@@ -0,0 +1,1 @@
+import{k as i}from"./tlpl-c-805Y60Cw.js";import"./tlpl-c-DQn8nPmW.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
