4086e3a8e89c334ceb851365f3d0b76e983dd310
genericness <107654710+genericness@users.noreply.github.com>
2026-09-06 02:42:34 +0000

    deploy 2026-09-06T02:42:34Z

---
+++ assets/imbj-c-DaU2kGYW.js
@@ -0,0 +1,1 @@
+import{k as i}from"./imbj-c-CYl31U29.js";import"./imbj-c-BqB4uHNC.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
