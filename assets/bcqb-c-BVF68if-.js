0504d86b2f23a6de2d391511f6b3f1cf986cb858
genericness <107654710+genericness@users.noreply.github.com>
2026-09-16 06:16:00 +0000

    deploy 2026-09-16T06:16:00Z

---
+++ assets/bcqb-c-BVF68if-.js
@@ -0,0 +1,1 @@
+import{k as i}from"./bcqb-c-N687aC6N.js";import"./bcqb-c-CSfONwmP.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
