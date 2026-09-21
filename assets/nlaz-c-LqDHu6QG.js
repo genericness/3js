b86c7e18c8e7abc78dc2d5ec95e71d42cd56aba6
genericness <107654710+genericness@users.noreply.github.com>
2026-09-21 05:25:03 +0000

    deploy 2026-09-21T05:25:03Z

---
+++ assets/nlaz-c-LqDHu6QG.js
@@ -0,0 +1,1 @@
+import{k as i}from"./nlaz-c-D7VQh6DY.js";import"./nlaz-c-BP182wzp.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
