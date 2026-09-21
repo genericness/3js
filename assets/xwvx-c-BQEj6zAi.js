75ba9ae617b014f95ab0e5f520207e6e3b783250
genericness <107654710+genericness@users.noreply.github.com>
2026-09-21 05:28:21 +0000

    deploy 2026-09-21T05:28:21Z

---
+++ assets/xwvx-c-BQEj6zAi.js
@@ -0,0 +1,1 @@
+import{k as i}from"./xwvx-c-BIF8I6Xh.js";import"./xwvx-c-CuqrFOgw.js";let t=null;function s(){return t||(t=new i),t}async function f(e,c=8){try{const o=s(),r=`https://duckduckgo.com/ac/?q=${encodeURIComponent(e)}&type=list`,n=await o.fetch(r);return n.ok?(await n.json())[1]?.slice(0,c)||[]:[]}catch{return[]}}export{f as fetchAutocompleteSuggestions};
